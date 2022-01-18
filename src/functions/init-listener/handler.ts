8de3d2875854ef26f7dcbe3e4737ec7985711af0
Miles <miles@karmacheck.com>
2022-01-17 18:11:02 -0600

    added console log

--- src/functions/init-listener/handler.ts
+++ src/functions/init-listener/handler.ts
@@ -3,6 +3,7 @@ import { middyfy } from '@libs/lambda';
 import { APIGatewayEvent } from 'aws-lambda';
 
 const initListener = async (event:APIGatewayEvent) => {
+  console.log(event)
   return formatJSONResponse({
     response_type: "in_channel",
     text: 'A Wordle leaderboard has been created for this channel'
