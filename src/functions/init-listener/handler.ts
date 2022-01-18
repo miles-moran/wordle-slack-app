7131cefbced5cd1ed909eb9bb74354297b8cebb3
Miles <miles@karmacheck.com>
2022-01-17 22:40:23 -0600

    no idea

--- src/functions/init-listener/handler.ts
+++ src/functions/init-listener/handler.ts
@@ -2,10 +2,13 @@ import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 import { APIGatewayEvent } from 'aws-lambda';
 import { ScoreboardService } from "../../services/scoreboard-service"
+import { URLSearchParams } from "url"
 const initListener = async (event:APIGatewayEvent) => {
   console.log(event)
   const scoreboardService = new ScoreboardService();
-  
+  const params = new URLSearchParams(event.body)
+  console.log(params)
+  const ts = params.get('ts');
 
   const scoreboard = scoreboardService.getScoreboard(ts);
   if (scoreboard){
@@ -18,7 +21,7 @@ const initListener = async (event:APIGatewayEvent) => {
   const res = scoreboardService.createScoreboard({
     today: {},
     total: [],
-    ts: 'a'
+    ts
   })
 
   console.log(res)
