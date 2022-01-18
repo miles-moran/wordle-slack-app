b7664d9856d85810cf1bd6733e14f98d9a744ab8
Miles <miles@karmacheck.com>
2022-01-17 18:10:42 -0600

    removed ocnsole log

--- src/functions/init-listener/handler.ts
+++ src/functions/init-listener/handler.ts
@@ -3,8 +3,6 @@ import { middyfy } from '@libs/lambda';
 import { APIGatewayEvent } from 'aws-lambda';
 
 const initListener = async (event:APIGatewayEvent) => {
-  const body = JSON.parse(event.body)
-  console.log(body)
   return formatJSONResponse({
     response_type: "in_channel",
     text: 'A Wordle leaderboard has been created for this channel'
