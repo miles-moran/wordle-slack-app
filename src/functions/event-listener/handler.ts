e6f71145b63ddff92db826b731b15d46ae00e878
Miles <miles@karmacheck.com>
2022-01-18 21:02:23 -0600

    a

--- src/functions/event-listener/handler.ts
+++ src/functions/event-listener/handler.ts
@@ -3,6 +3,9 @@ import { middyfy } from '@libs/lambda';
 import { APIGatewayEvent } from 'aws-lambda';
 
 const eventListener = async (event:APIGatewayEvent) => {
+  console.log(event)
+  console.log(event.body)
+  console.log(typeof event.body)
   const body = JSON.parse(event.body);
   console.log(body)
   return formatJSONResponse({
