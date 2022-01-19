d94e72f4174d61c9558a23ce525898da78b99e13
Miles <miles@karmacheck.com>
2022-01-18 21:13:13 -0600

    a

--- src/functions/event-listener/handler.ts
+++ src/functions/event-listener/handler.ts
@@ -1,13 +1,9 @@
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-import { APIGatewayEvent } from 'aws-lambda';
 
-const eventListener = async (event:APIGatewayEvent) => {
-  console.log(event)
-  console.log(event.body)
-  console.log(typeof event.body)
-  const body = JSON.parse(event.body);
-  console.log(body)
+const eventListener = async (event:any) => {
+  const body = event.body as any;
+  const t
   return formatJSONResponse({
     message: `Hello ${event.body}, welcome to the exciting Serverless world!`,
     challenge: body.challenge,
