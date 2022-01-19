f7ac326fc4cf23e2727672ff94af0a42196e5f13
Miles <miles@karmacheck.com>
2022-01-18 21:30:14 -0600

    fixed

--- src/functions/event-listener/handler.ts
+++ src/functions/event-listener/handler.ts
@@ -4,9 +4,7 @@ import { middyfy } from '@libs/lambda';
 const eventListener = async (event) => {
   const body = event.body as any;
   return formatJSONResponse({
-    message: `Hello ${event.body}, welcome to the exciting Serverless world!`,
-    challenge: body.challenge,
-    event,
+    challenge: body.challenge
   });
 }
 
