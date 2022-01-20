545fecb743ebff25f4e5a19064c1f0d3b46724a3
Miles <miles@karmacheck.com>
2022-01-19 18:04:08 -0600

    body

--- src/functions/events/discord/handler.ts
+++ src/functions/events/discord/handler.ts
@@ -5,6 +5,8 @@ import * as nacl from "tweetnacl";
 
 const discord = async (event: { body: any; }) => {
   const body = event.body as any;
+  console.log(event)
+  console.log('body')
   console.log(body)
   const PUBLIC_KEY = 'b966846f7daf49b55bf378ee288b927507621ae0b4171a6e9ba5e1a95830f9fb';
   const params = new URLSearchParams(event.body)
